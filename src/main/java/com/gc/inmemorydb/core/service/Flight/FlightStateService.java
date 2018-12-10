d36dbf0369eb184f3ae01b30d308d2d67299ced8
zhenzhenz <1124599962@qq.com>
2018-12-10 15:12:38 +0800

    flight state

---
+++ src/main/java/com/gc/inmemorydb/core/service/Flight/FlightStateService.java
@@ -0,0 +1,15 @@
+package com.gc.inmemorydb.core.service.Flight;
+
+import com.gc.inmemorydb.core.dto.system.flight.GetFlightStateDTO;
+import com.gc.inmemorydb.core.entity.system.Flight;
+import com.baomidou.mybatisplus.plugins.Page;
+
+public interface FlightStateService {
+
+    /**
+     * 获取航班状态信息
+     * @return
+     **/
+    Page<Flight> getFlightStatus(GetFlightStateDTO getFlightStateDTO);
+
+}
