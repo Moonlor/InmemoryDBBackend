1d747faf5ee11d2da848a71e32aa4d1cb8fb3569
Tianyou Song <songtianyou07@gmail.com>
2018-12-10 22:20:11 +0800

    订单创建

---
+++ src/main/java/com/gc/inmemorydb/core/entity/system/Ticketorder.java
@@ -0,0 +1,21 @@
+package com.gc.inmemorydb.core.entity.system;
+
+import com.baomidou.mybatisplus.annotations.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+@Data
+
+public class Ticketorder implements Serializable {
+
+
+    private Integer orderId;
+
+    private Integer userId;
+
+    private Integer flightId;
+
+    private String  status;
+}
